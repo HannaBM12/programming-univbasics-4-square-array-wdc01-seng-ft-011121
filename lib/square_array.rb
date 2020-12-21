383315ff2228bfb0c8ede5868d9e014387f54edc
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-12-21 18:33:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,13 @@
 def square_array(array)
-  counter = 0
+  # counter = 0
   new_array = []
-  while counter < array.length do
-    new_array.push array[counter] ** 2
-    counter +=1
-  end
+  # while counter < array.length do
+  #   new_array.push array[counter] ** 2
+  #   counter +=1
+  # end
+  # new_array
+  array.length.times { |element|
+    new_array.push array[element] ** 2
+  }
   new_array
 end
